d6db19bcb2a007e7dd7ac0050a639f22829a3b09
coco227 <coco@ZhangcocodeMacBook-Pro.local>
2016-03-22 22:58:08 +0800

    创建表格

---
+++ mySQLDemo/setup.js
@@ -0,0 +1,41 @@
+/**模块依赖*/
+var mysql = require('mysql') , 
+	config = require('./config')
+
+/**初始化客户端*/
+delete config.database;
+var db = mysql.createClient(config);
+
+db.on('error', function(){
+
+});
+
+/**创建数据库*/
+db.query('CREATE DATABASE IF NOT EXISTS `cart-example`');
+db.query('USE `cart-example`');
+
+/**创建表*/
+db.query('DROP TABLE IF EXISTS item');
+	db.query('CREATE TABLE item (' + 
+			 'id INT(11) AUTO_INCREMENT,' +
+			 'title VARCHAR(255),' + 
+			 'description TEXT,' +
+			 'created DATETIME,' + 
+			 'PRIMARY KEY (id))'
+			);
+db.query('DROP TABLE IF EXISTS review');
+	db.query('CREATE TABLE review (' +
+			 'id INT(11) AUTO_INCREMENT,' +
+			 'item_id INT(11),' + 
+			 'text TEXT,' +
+			 'stars INT(1),' + 
+			 'created DATETIME,' + 
+			 'PRIMARY KEY (id))'
+			);
+
+/**关闭客户端*/
+db.end();
+
+db.end(function(){
+	process.exit();
+});
